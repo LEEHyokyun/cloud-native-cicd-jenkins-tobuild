a466492b21efa07846f4cf67c5ab4805dc2a82e9
leehyokyun <gyrbs22@gmail.com>
2026-03-23 12:04:05 +0900

    cloud-native-cicd-jenkins-tobuild project init

---
+++ readme.md
@@ -0,0 +1,10 @@
+## 1.  개요
+
+- Java ver 21.
+- Spring Boot ver 3.5.0 
+
+## 2. Projects
+
+- Jenkins cicd pipleline 구축을 위한 프로젝트
+  - tobuild
+
---
+++ src/main/java/com/cicd/jenkins/user/CicdJenkinsTobuildApplication.java
@@ -0,0 +1,11 @@
+package com.cicd.jenkins.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdJenkinsTobuildApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CicdJenkinsTobuildApplication.class, args);
+    }
+}
